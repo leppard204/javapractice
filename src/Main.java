f1069ff2678261f1caabd79a3e3dbe78f2eb2c5b
leppard204 <eyela1004@naver.com>
2023-10-03 15:36:12 +0900

    ij first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++)
+        {
+            System.out.println("i = " + i);
+        }
+    }
+}
